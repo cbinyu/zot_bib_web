c520c96afe32b8aa64d764e3d1e41d78f12b3567
chrysapa <chrysa.papadaniil@gmail.com>
2021-04-21 11:54:46 -0400

    CBI publications format

--- demo/settings4.py
+++ demo/settings4.py
@@ -2,24 +2,24 @@
 
 #### You must configure the collection(s) to be loaded
 
-group_collection(160464, collection='MGID93AS')
+group_collection(2892272, api_key=None, collection=None, top_level=False)   #group library
 
 # 160464 #  group ID (e.g., six numeric digits)
 # 'MGID93AS'  # collection where to find the bibliographic items.
 # (You can load a user collection with user_collection().)
 
 # Load an additional collection from a different library:
-user_collection('1366641', api_key = 'X4iUU2a7P5mQWNTO7hvuQwzB', collection='FCQK6PT6')
-exclude_collection('FCQK6PT6', top_level_only=True) # Do not keep top-level items from this collection
+#user_collection('1366641', api_key = 'X4iUU2a7P5mQWNTO7hvuQwzB', collection='FCQK6PT6')
+#exclude_collection('FCQK6PT6', top_level_only=True) # Do not keep top-level items from this collection
 
 # One could also include the top level collections
 #exclude_collection('FCQK6PT6', top_level_only=True)  # exclude items at the top level (redundant)
-exclude_collection('Selected Works') # Selected Works ZJFRZTCB
-exclude_collection('AWMEXUV2') # In Review and to appear
-exclude_collection('HZHSMP74') # Theses
-rename_collection('BQUUP5PR', "Language")
+#exclude_collection('Selected Works') # Selected Works ZJFRZTCB
+#exclude_collection('AWMEXUV2') # In Review and to appear
+#exclude_collection('HZHSMP74') # Theses
+#rename_collection('BQUUP5PR', "Language")
 
-exclude_items(lambda i: 'Reitter' in i.html and i.year<2011)  # This is a function that returns True for every item to be excluded
+#exclude_items(lambda i: 'Reitter' in i.html and i.year<2011)  # This is a function that returns True for every item to be excluded
 
 #### Optional Settings
 
@@ -37,11 +37,19 @@ exclude_items(lambda i: 'Reitter' in i.html and i.year<2011)  # This is a functi
 
 ###### Special settings
 
-bib_style = 'mla'     # bibliography style format (e.g., 'apa' or 'mla') - Any valid CSL style in the Zotero style repository
+sort_criteria = ['-date','type']
+
+titlestring = 'CBI Publications'
+""" bibliography title """
+
+show_top_section_headings = True
+""" show section headings for the primary search criterion"""
+
+bib_style = 'apa'     # bibliography style format (e.g., 'apa' or 'mla') - Any valid CSL style in the Zotero style repository
 
 write_full_html_header = True   # False to not output HTML headers.  In this case, expect a file in UTF-8 encoding.
 
-outputfile = 'demo/example4.html'  # relative or absolute path name of output file
+outputfile = 'zotero_bib_style4.html'  # relative or absolute path name of output file
 
 show_search_box = True  # show a Javascript/JQuery based search box to filter pubs by keyword.  Must define jquery_path.
 jquery_path = "site/jquery.min.js"  # path to jquery file on the server - default: wordpress location
@@ -53,6 +61,8 @@ copy_button_path = "site/clippy.svg" # path to file on server
 
 show_links = ['abstract', 'pdf', 'bib', 'wikipedia', 'endnote', 'coins']
 
-show_shortcuts = ['type',shortcut('collection', sortBy='name')]
+show_shortcuts = ['year']
+show_shortcuts += [shortcut('venue_short', sortDir='desc',
+                            sortBy='count', topN=10)]
 
 stylesheet_url = "style4.css"
